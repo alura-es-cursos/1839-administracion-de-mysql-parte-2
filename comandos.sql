3b6327cda708e4f2fcf4a8e008a23f516ef13a83
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-07-26 17:27:36 -0300

    Comandos Aula 5

--- comandos.sql
+++ comandos.sql
@@ -113,3 +113,23 @@ SELECT * FROM items_facturas;
 
 INSERT INTO tabla_de_productos1
 SELECT * FROM tabla_de_productos;
+
+
+/* PROYECTO DEL AULA ANTERIOR 4*/
+
+
+CREATE USER 'admin02'@'localhost' IDENTIFIED BY 'admin02';
+
+GRANT ALL PRIVILEGES ON *.* TO 'admin02'@'localhost' WITH GRANT OPTION;
+
+CREATE USER 'user02'@'localhost' IDENTIFIED BY 'user02';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE TEMPORARY TABLES, LOCK TABLES, EXECUTE ON *.* TO 'user02'@'localhost';
+
+CREATE USER 'read01'@'localhost' IDENTIFIED BY 'read01';
+
+GRANT SELECT, EXECUTE ON *.* TO 'read01'@'localhost';
+
+CREATE USER 'back01'@'localhost' IDENTIFIED BY 'back01';
+
+GRANT SELECT, RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'back01'@'localhost';
