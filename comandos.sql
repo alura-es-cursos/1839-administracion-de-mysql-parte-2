c7f54799e745456a7605f356fb25a74c66e2789c
Alvaro Camacho <60886111+ahcamachod@users.noreply.github.com>
2021-07-27 10:58:08 -0300

    Comandos Proyecto Final

--- comandos.sql
+++ comandos.sql
@@ -133,3 +133,29 @@ GRANT SELECT, EXECUTE ON *.* TO 'read01'@'localhost';
 CREATE USER 'back01'@'localhost' IDENTIFIED BY 'back01';
 
 GRANT SELECT, RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'back01'@'localhost';
+
+
+
+/* PROYECTO FINAL*/
+
+
+CREATE USER 'admingeneric02'@'%' IDENTIFIED BY 'admingeneric02';
+
+GRANT ALL PRIVILEGES ON *.* TO 'admingeneric02'@'%' WITH GRANT OPTION;
+
+CREATE USER 'user04'@'%' IDENTIFIED BY 'user04';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE TEMPORARY TABLES,
+LOCK TABLES, EXECUTE ON jugos_ventas.* TO 'user04'@'%';
+
+CREATE USER 'user05'@'%' IDENTIFIED BY 'user05';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON jugos_ventas.facturas TO 'user05'@'%';
+
+GRANT SELECT ON jugos_ventas.tabla_de_vendedores TO 'user05'@'%';
+
+SELECT * FROM mysql.user;
+
+SHOW GRANTS FOR 'user02'@'localhost';
+
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user02'@'localhost';
